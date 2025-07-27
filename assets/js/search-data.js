7f54f69f898e071094467e0924af358d5519b636
vraneskoy <168984990+vraneskoy@users.noreply.github.com>
2025-07-27 03:04:54 +0000

    Deploying to gh-pages from @ vraneskoy/vraneskoy.github.io@d3f245ed6cd5d086a0c41ebbc33d111ef8783cbb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,12 +23,26 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-projects",
+          title: "projects",
+          description: "A growing collection of your cool projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
         },{id: "nav-cv",
           title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
+          },
+        },{id: "nav-people",
+          title: "people",
+          description: "members of the lab or group",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/people/";
           },
         },{id: "dropdown-blog",
               title: "blog",
